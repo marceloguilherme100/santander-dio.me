b5dd5b94cb70daefd952d9d79252e82ef8f1af72
marceloguilherme100 <marcelo.guilherme100@gmail.com>
2024-05-04 23:49:38 -0300

    operadores e variaveis

---
+++ tipos-variaves/src/Operadores.java
@@ -0,0 +1,21 @@
+import java.sql.Date;
+
+public class Operadores {
+    public static void main(String[] args) {
+        String nome = "Gui";
+        int idade = 22;
+        double peso = 79.8;
+        char sexo = 'M';
+        boolean doadorOrgao = false;
+        //Date dataNascimento = new Date();
+
+        String nomeCompleto = "LIGUAGEM " + "JAVA";
+
+        System.out.println(nomeCompleto);
+
+        /* System.out.println(idade);
+        System.out.println(peso);
+        System.out.println(sexo);
+        System.out.println(doadorOrgao); */
+    }
+}
--- tipos-variaves/src/TiposVariaveis.java
+++ tipos-variaves/src/TiposVariaveis.java
@@ -1,5 +1,10 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500;
+        int numero = 10;
+        final int NUX = 20;
+        String meuNome = "Marcelo";
+        numero = 8;
+        System.out.println(meuNome);
     }
 }
